c750afed79f796fdff61328c5db81c8e3d9123a4
Artemiadze <t0mi.v@yandex.ru>
2025-06-21 16:35:52 +0300

    Make Handler using Echo framework

---
+++ cmd/main/main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	fmt.Println("Server is running...")
+
+	s := echo.New()
+	// Задаём параметры обработчика
+	s.GET("/status", Handler)
+
+	err := s.Start(":8080")
+	if err != nil {
+		// команда s.Start может возвращать ошибку
+		log.Fatal(err)
+		return
+	}
+}
+
+func Handler(c echo.Context) error {
+	// обьект дата
+	d := time.Date(2027, time.January, 1, 0, 0, 0, 0, time.UTC)
+
+	dur := time.Until(d) // Вычисляем время до указанной даты
+	s := fmt.Sprintf("Времени до 1.01.2027: %d\n", int64(dur.Hours()/24))
+
+	err := c.String(http.StatusOK, s) // Возвращаем статус 200 OK
+	if err != nil {
+		// Если произошла ошибка при отправке ответа, логируем её
+		return err
+	}
+	return nil
+}
